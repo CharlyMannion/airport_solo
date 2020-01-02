ec6f814bca7ab7ad9c6c47eed4a4c007d3627a66
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-02 18:13:34 +0000

    error - atc's airport is nil

--- lib/air_traffic_control.rb
+++ lib/air_traffic_control.rb
@@ -1,3 +1,9 @@
+require_relative 'airport'
+
 class AirTrafficControl
   attr_accessor :airport, :plane
+
+  def initalize(airport = Airport.new)
+    @airport = airport
+  end
 end
--- spec/air_traffic_control_spec.rb
+++ spec/air_traffic_control_spec.rb
@@ -1,4 +1,5 @@
 require 'air_traffic_control'
+require 'airport'
 
 describe AirTrafficControl do
   it 'responds to airport' do
@@ -6,6 +7,20 @@ describe AirTrafficControl do
   end
   it 'responds to plane' do
     expect(subject).to respond_to :plane
+  end
+  it 'responds to initialize' do
+    expect(subject).to respond_to :initalize
+  end
+
+  describe '#initalize' do
+    it 'has an instance of the aiport class' do
+      airport_double = double :airport
+      # airport_class_double  = double :airport_class, new: airport_double
+      allow(subject).to receive(:new).and_return(airport_double)
+      p "subject below"
+      p subject
+      expect(subject.airport).to eq(airport_double)
+    end
   end
 
 end
