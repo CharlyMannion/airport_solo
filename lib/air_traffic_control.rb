7764ac3adc66af01c9aecbc65210ad4dd5edde74
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-03 11:44:46 +0000

    creates land method on airtrafficcontrol

--- lib/air_traffic_control.rb
+++ lib/air_traffic_control.rb
@@ -1,4 +1,6 @@
 require_relative 'airport'
+require_relative 'plane'
+
 
 class AirTrafficControl
   attr_accessor :airport, :plane
@@ -6,5 +8,9 @@ class AirTrafficControl
   def initialize(airport_class = Airport)
     @airport_class = airport_class
     @airport = @airport_class.new
+  end
+
+  def land(plane = Plane.new, airport)
+    true 
   end
 end
--- spec/air_traffic_control_spec.rb
+++ spec/air_traffic_control_spec.rb
@@ -1,5 +1,6 @@
 require 'air_traffic_control'
 require 'airport'
+require 'plane'
 
 describe AirTrafficControl do
   it 'responds to airport' do
@@ -14,6 +15,17 @@ describe AirTrafficControl do
       airport_double = double :airport
       airport_class_double  = double :airport_class, new: airport_double
       allow(AirTrafficControl).to receive(:new).and_return(airport_double)
+    end
+  end
+
+  describe '#land' do
+    it 'should land a plane at an airport' do
+      airport_double = double :airport
+      airport_class_double  = double :airport_class, new: airport_double
+      atc = AirTrafficControl.new(airport_class_double)
+      plane_double = double :plane
+      p atc.airport
+      expect(atc.land(plane_double, atc.airport)).to eq(true)
     end
   end
 
