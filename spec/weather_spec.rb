ad7d647257a7b5550490073777ed3b5d49d14516
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-05 14:12:19 +0000

    refactors weather spec

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,17 +1,18 @@
 require 'weather'
 
 describe Weather do
+  subject(:weather) { described_class.new }
+
   describe '#initialize' do
     it 'should have a default  weather condition of fine' do
-      weather = Weather.new
       expect(weather.condition).to eq(:fine)
     end
   end
 
   describe 'check_weather' do
     it 'should change the weather condition' do
-      p allow(subject).to receive(:check_weather) { :rain }
-      p subject.condition
+      p allow(weather).to receive(:check_weather) { :rain }
+      p weather.condition
       # p expect(subject.condition).to eq(:rain)
     end
   end
